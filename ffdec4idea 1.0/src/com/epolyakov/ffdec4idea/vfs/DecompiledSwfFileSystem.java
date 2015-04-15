0cf2dec3fd98e61515f8d0ddeae4001d253ec8e3
evgeniy-polyakov <evgeniy.s.polyakov@gmail.com>
2015-04-15 10:24:23 +0600

    Finished implementation of DecompiledSwfFileSystem.

--- ffdec4idea 1.0/src/com/epolyakov/ffdec4idea/vfs/DecompiledSwfFileSystem.java
+++ ffdec4idea 1.0/src/com/epolyakov/ffdec4idea/vfs/DecompiledSwfFileSystem.java
@@ -1,5 +1,6 @@
 package com.epolyakov.ffdec4idea.vfs;
 
+import com.intellij.openapi.util.io.BufferExposingByteArrayInputStream;
 import com.intellij.openapi.util.io.FileAttributes;
 import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.openapi.vfs.LocalFileSystem;
@@ -20,6 +21,7 @@ import java.util.ResourceBundle;
  */
 public class DecompiledSwfFileSystem extends NewVirtualFileSystem {
 
+    public static final String PROTOCOL = "swf";
     public static final String SWF_SEPARATOR = "!/";
     private static ResourceBundle resources = ResourceBundle.getBundle("com.epolyakov.ffdec4idea.resources.ffdec4idea");
 
@@ -57,119 +59,125 @@ public class DecompiledSwfFileSystem extends NewVirtualFileSystem {
 
     @NotNull
     @Override
-    public InputStream getInputStream(@NotNull VirtualFile _virtualFile) throws IOException {
-        return null;
+    public InputStream getInputStream(@NotNull VirtualFile file) throws IOException {
+        return new BufferExposingByteArrayInputStream(contentsToByteArray(file));
     }
 
     @NotNull
     @Override
-    public OutputStream getOutputStream(@NotNull VirtualFile _virtualFile, Object o, long l, long l1)
+    public OutputStream getOutputStream(@NotNull VirtualFile file, Object requestor, long modStamp, long timeStamp)
             throws IOException {
-        return null;
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
     }
 
     @Override
-    public long getLength(@NotNull VirtualFile _virtualFile) {
-        return 0;
+    public long getLength(@NotNull VirtualFile file) {
+        try {
+            return contentsToByteArray(file).length;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0L;
+        }
     }
 
     @Override
-    public boolean exists(@NotNull VirtualFile _virtualFile) {
+    public boolean exists(@NotNull VirtualFile file) {
+        return getHandler(file).exists(getRelativePath(file));
+    }
+
+    @NotNull
+    @Override
+    public String[] list(@NotNull VirtualFile file) {
+        return getHandler(file).list(getRelativePath(file));
+    }
+
+    @Override
+    public boolean isDirectory(@NotNull VirtualFile file) {
+        return getHandler(file).isDirectory(getRelativePath(file));
+    }
+
+    @Override
+    public long getTimeStamp(@NotNull VirtualFile file) {
+        return -1L;
+    }
+
+    @Override
+    public void setTimeStamp(@NotNull VirtualFile file, long timeStamp) throws IOException {
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
+    }
+
+    @Override
+    public boolean isWritable(@NotNull VirtualFile file) {
         return false;
     }
 
-    @NotNull
     @Override
-    public String[] list(@NotNull VirtualFile _virtualFile) {
-        return new String[0];
-    }
-
-    @Override
-    public boolean isDirectory(@NotNull VirtualFile _virtualFile) {
-        return false;
-    }
-
-    @Override
-    public long getTimeStamp(@NotNull VirtualFile _virtualFile) {
-        return 0;
-    }
-
-    @Override
-    public void setTimeStamp(@NotNull VirtualFile _virtualFile, long l) throws IOException {
-
-    }
-
-    @Override
-    public boolean isWritable(@NotNull VirtualFile _virtualFile) {
-        return false;
-    }
-
-    @Override
-    public void setWritable(@NotNull VirtualFile _virtualFile, boolean b) throws IOException {
-
+    public void setWritable(@NotNull VirtualFile file, boolean writableFlag) throws IOException {
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
     }
 
     @NotNull
     @Override
-    public VirtualFile createChildDirectory(Object o, @NotNull VirtualFile _virtualFile, @NotNull String s)
+    public VirtualFile createChildDirectory(Object requestor, @NotNull VirtualFile parent, @NotNull String dir)
             throws IOException {
-        return null;
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), parent.getUrl()));
     }
 
     @NotNull
     @Override
-    public VirtualFile createChildFile(Object o, @NotNull VirtualFile _virtualFile, @NotNull String s)
+    public VirtualFile createChildFile(Object requestor, @NotNull VirtualFile parent, @NotNull String dir)
             throws IOException {
-        return null;
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), parent.getUrl()));
     }
 
     @NotNull
     @Override
     public String getProtocol() {
-        return null;
+        return PROTOCOL;
     }
 
     @Nullable
     @Override
-    public VirtualFile findFileByPath(@NotNull String s) {
-        return null;
+    public VirtualFile findFileByPath(@NotNull String path) {
+        return VfsImplUtil.findFileByPath(this, path);
     }
 
     @Override
-    public void refresh(boolean b) {
-
+    public void refresh(boolean asynchronous) {
+        VfsImplUtil.refresh(this, asynchronous);
     }
 
     @Nullable
     @Override
-    public VirtualFile refreshAndFindFileByPath(@NotNull String s) {
-        return null;
+    public VirtualFile refreshAndFindFileByPath(@NotNull String path) {
+        return VfsImplUtil.refreshAndFindFileByPath(this, path);
     }
 
     @Override
-    public void deleteFile(Object o, @NotNull VirtualFile _virtualFile) throws IOException {
-
+    public void deleteFile(Object requestor, @NotNull VirtualFile file) throws IOException {
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
     }
 
     @Override
-    public void moveFile(Object o, @NotNull VirtualFile _virtualFile, @NotNull VirtualFile _virtualFile1)
+    public void moveFile(Object o, @NotNull VirtualFile file, @NotNull VirtualFile newParent)
             throws IOException {
-
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
     }
 
     @Override
-    public void renameFile(Object o, @NotNull VirtualFile _virtualFile, @NotNull String s) throws IOException {
-
+    public void renameFile(Object o, @NotNull VirtualFile file, @NotNull String newName) throws IOException {
+        throw new IOException(MessageFormat.format(resources.getString("swf.modification.not.supported.error"), file.getUrl()));
     }
 
     @Nullable
     @Override
-    public FileAttributes getAttributes(@NotNull VirtualFile _virtualFile) {
-        return null;
+    public FileAttributes getAttributes(@NotNull VirtualFile file) {
+        return new FileAttributes(isDirectory(file), false, false, false, getLength(file), 0L, false);
     }
 
     @NotNull
-    private SwfHandler getHandler(@NotNull VirtualFile file) throws IOException {
+    private SwfHandler getHandler(@NotNull VirtualFile file) {
+        // todo add cache
         return new SwfHandler(file);
     }
 
--- ffdec4idea 1.0/src/com/epolyakov/ffdec4idea/vfs/SwfHandler.java
+++ ffdec4idea 1.0/src/com/epolyakov/ffdec4idea/vfs/SwfHandler.java
@@ -1,6 +1,7 @@
 package com.epolyakov.ffdec4idea.vfs;
 
 import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.util.ArrayUtil;
 import com.jpexs.decompiler.flash.SWF;
 import com.jpexs.decompiler.flash.abc.ScriptPack;
 import com.jpexs.decompiler.flash.configuration.Configuration;
@@ -21,10 +22,10 @@ public class SwfHandler {
     private SWF swf;
     private List<ScriptPack> scriptPacks;
 
-    public SwfHandler(VirtualFile file) throws IOException {
+    public SwfHandler(VirtualFile file) {
         try {
             swf = new SWF(file.getInputStream(), false);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | IOException e) {
             e.printStackTrace();
         }
     }
@@ -45,15 +46,72 @@ public class SwfHandler {
         }
     }
 
-    private ScriptPack getScriptPackByQName(@NotNull String qName) {
-        if (scriptPacks == null) {
-            scriptPacks = swf.getAS3Packs().stream().map(MyEntry::getValue).collect(Collectors.toList());
-        }
-        return scriptPacks.stream().filter(s -> s.getClassPath().toString().equals(qName)).findFirst().get();
+    public boolean exists(@NotNull String relativePath) {
+        String qName = getQNameByPath(relativePath);
+        return isPackageOrClass(qName);
+    }
+
+    public String[] list(@NotNull String relativePath) {
+        String qName = getQNameByPath(relativePath);
+        return getPackageContents(qName);
+    }
+
+    public boolean isDirectory(@NotNull String relativePath) {
+        String qName = getQNameByPath(relativePath);
+        return isPackage(qName);
     }
 
     @NotNull
     private String getQNameByPath(@NotNull String path) {
         return path.replace('/', '.');
+    }
+
+    private ScriptPack getScriptPackByQName(@NotNull String qName) {
+        setScriptPacks();
+        return scriptPacks.stream()
+                .filter(s -> s.getClassPath().toString().equals(qName))
+                .findFirst().get();
+    }
+
+    private boolean isPackageOrClass(@NotNull String qName) {
+        setScriptPacks();
+        String packageName = qName + ".";
+        return scriptPacks.stream()
+                .map(s -> s.getClassPath().toString())
+                .anyMatch(s -> s.equals(qName) || s.startsWith(packageName));
+    }
+
+    private boolean isPackage(@NotNull String qName) {
+        setScriptPacks();
+        String packageName = qName + ".";
+        return scriptPacks.stream()
+                .map(s -> s.getClassPath().toString())
+                .anyMatch(s -> s.startsWith(packageName));
+    }
+
+    private String[] getPackageContents(@NotNull String qName) {
+        setScriptPacks();
+        String packageName = qName + ".";
+        int packageNameLength = packageName.length();
+        List<String> containedClasses = scriptPacks.stream()
+                .map(s -> s.getClassPath().toString())
+                .filter(s -> s.startsWith(packageName))
+                .collect(Collectors.toList());
+        containedClasses.replaceAll(s -> {
+            int i = s.indexOf('.', packageNameLength);
+            if (i > packageNameLength) {
+                s = s.substring(packageNameLength, i);
+            } else {
+                s = s.substring(packageNameLength);
+            }
+            return s;
+        });
+        return ArrayUtil.toStringArray(containedClasses);
+    }
+
+    private void setScriptPacks() {
+        if (scriptPacks == null) {
+            scriptPacks = swf.getAS3Packs().stream().map(MyEntry::getValue).collect(Collectors.toList());
+        }
     }
 }
